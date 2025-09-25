6f416233211cc7bddc01b4ec3160c120122c882d
haemin <goalsgoals0417@gmail.com>
2025-09-26 00:34:12 +0900

    feat: Tailwind v4 환경 설정 및 커스텀 색상(@theme) 적용

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom-white': '#fefefe',
+                'custom-gray': '#f9fafb',
+                'custom-blue': '#5aa1f6',
+            }
+        },
+    },
+    plugins: [],
+}
