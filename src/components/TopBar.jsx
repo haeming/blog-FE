70546ba9c30814a9ae689cf95509dd3451caf1e2
haemin <goalsgoals0417@gmail.com>
2025-09-27 13:00:39 +0900

    feat(ui): TopBar UI 개선 (메뉴 정렬 및 border 추가)

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,11 +7,19 @@ export default function TopBar(){
     return(
         <div className="top-0 z-50">
             <div className="mx-auto flex items-center justify-between p-4">
-                <div>
-                    <div className="flex flex-col items-center justify-center cursor-pointer" onClick={pageService.goToHome}>
-                        <img src="/logo/logo16.png" className="w-15" alt="logo"/>
-                        <span>HaemStory</span>
-                    </div>
+                <div className="flex flex-col items-center justify-center cursor-pointer"
+                     onClick={pageService.goToHome}>
+                    <img src="/logo/logo16.png" className="w-8" alt="logo"/>
+                    <span>HaemStory</span>
+                </div>
+                <div className="flex items-center justify-center space-x-10 text-topmenu font-bold">
+                    <span className="cursor-pointer hover:text-custom-purple">DASHBOARD</span>
+                    <span className="cursor-pointer hover:text-custom-purple">POSTS</span>
+                    <span className="cursor-pointer hover:text-custom-purple">CATEGORIES</span>
+                    <span className="cursor-pointer hover:text-custom-purple">COMMENTS</span>
+                </div>
+                <div className="flex">
+                    <span className="cursor-pointer hover:text-custom-purple">Logout</span>
                 </div>
             </div>
         </div>
