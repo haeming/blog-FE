3d7da0825aefd192a11bef07a3379ccfb11788a4
haemin <goalsgoals0417@gmail.com>
2025-10-14 12:11:06 +0900

    feat: token 만료 처리 확인 로직 추가

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,3 +1,9 @@
-export default function adminApi(){
+import getAuthHeaders from "../commons/utils/authHeaders";
+import { request } from "./request";
 
+export const adminApi = {
+    verifyToken: async () => {
+        const config = getAuthHeaders();
+        return await request("get", "/api/admin/verify-token", null, config);
+    }
 }
