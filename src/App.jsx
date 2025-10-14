1b1dd28e3d295230542753e378b75f50a4ab02ee
haemin <goalsgoals0417@gmail.com>
2025-10-14 12:23:05 +0900

    feat: App 초기 마운트 시 토큰 검증 및 로그인 상태 복원 로직 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,34 @@ import './App.css'
 import {BrowserRouter, Routes} from "react-router-dom";
 import {AppRouter} from "./routes/AppRouter.jsx";
 import {ToastContainer} from "react-toastify";
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { adminApi } from './api/adminApi.js';
+import { loginAction } from './store/authSlice.js';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+        if(token){
+          try {
+            const res = await adminApi.verifyToken();
+            const {accountName} = res.data.result;
+            dispatch(loginAction({accountName, token}));
+          } catch (err){
+            console.error(err);
+            localStorage.removeItem("token");
+            localStorage.removeItem("accountName");
+          }
+
+        }
+    }
+    checkToken();
+
+  }, [dispatch])
 
   return (
     <>
