10e0fabf2763c02b5b9e0ccb4b03cf09ca3da8be
haemin <goalsgoals0417@gmail.com>
2025-10-22 12:40:22 +0900

    feat: react 19 -> 17 버전으로 다운

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter, Routes} from "react-router-dom";
 import {AppRouter} from "./routes/AppRouter.jsx";
 import {ToastContainer} from "react-toastify";
